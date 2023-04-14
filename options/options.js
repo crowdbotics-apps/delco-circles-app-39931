cfaf787ccf6f2a4b35e791f313ab116127a731e1
Crowdbotics <team@crowdbotics.com>
2023-04-14 13:41:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delco-circles-app-39931",
+  url: "https://delco-circles-app-39931.botics.co",
+  api: "https://delco-circles-app-39931.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
