cc3e7c89791e36c821f3790d51e7133013c047d4
John Bender <john.b@crowdbotics.com>
2023-04-14 09:45:45 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "delco-circles-app-39931",
-  url: "https://delco-circles-app-39931.botics.co",
-  api: "https://delco-circles-app-39931.botics.co/api/v1"
-}
+  "name": "delco-circles-app-39931",
+  "url": "https://delco-circles-app-39931.botics.co",
+  "api": "https://delco-circles-app-39931.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
